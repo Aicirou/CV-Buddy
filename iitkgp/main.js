ee530d4029acc034dde029ac9befa85e5cc9b205
Aicirou <ajaykumarmeena808@gmail.com>
2024-05-05 17:01:29 +0530

    Update CV data saving code in iitm and iitkgp main.js files

--- iitkgp/main.js
+++ iitkgp/main.js
@@ -1,19 +1,18 @@
-
 function initCv() {
-    cvData = JSON.parse(localStorage.getItem('iitkgpCvData'));
-    if(cvData){
-        cv.innerHTML = cvData;
-    }
+  cvData = JSON.parse(localStorage.getItem("iitkgpCvData"));
+  if (cvData) {
+    cv.innerHTML = cvData;
+  }
 }
 
 initCv();
 
-$('#saveCv').on('click', (event) => {
-    cv.removeAttribute('contenteditable');
-    localStorage.setItem('iitkgpCvData', JSON.stringify(cv.innerHTML));
+$("#saveCv").on("click", (event) => {
+  cv.removeAttribute("contenteditable");
+  localStorage.setItem("iitkgpCvData", JSON.stringify(cv.innerHTML));
 });
 
-$('#resetCv').on('click', (event) => {
-    localStorage.removeItem('iitkgpCvData');
-    location.reload();
+$("#resetCv").on("click", (event) => {
+  localStorage.removeItem("iitkgpCvData");
+  location.reload();
 });
--- iitm/main.js
+++ iitm/main.js
@@ -1,19 +1,18 @@
-
 function initCv() {
-    cvData = JSON.parse(localStorage.getItem('iitmCvData'));
-    if(cvData){
-        cv.innerHTML = cvData;
-    }
+  cvData = JSON.parse(localStorage.getItem("iitmCvData"));
+  if (cvData) {
+    cv.innerHTML = cvData;
+  }
 }
 
 initCv();
 
-$('#saveCv').on('click', (event) => {
-    cv.removeAttribute('contenteditable');
-    localStorage.setItem('iitmCvData', JSON.stringify(cv.innerHTML));
+$("#saveCv").on("click", (event) => {
+  cv.removeAttribute("contenteditable");
+  localStorage.setItem("iitmCvData", JSON.stringify(cv.innerHTML));
 });
 
-$('#resetCv').on('click', (event) => {
-    localStorage.removeItem('iitmCvData');
-    location.reload();
+$("#resetCv").on("click", (event) => {
+  localStorage.removeItem("iitmCvData");
+  location.reload();
 });
